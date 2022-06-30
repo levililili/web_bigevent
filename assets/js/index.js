3b8e618f0a868a8fc116e525efe90ce0733237f4
levililili <593494294@qq.com>
2022-06-30 18:57:58 +0800

    封装了获取用户的信息的函数

--- assets/js/index.js
+++ assets/js/index.js
@@ -37,24 +37,24 @@ $(function () {
         }
     }
 
-    // 获取用户的信息
-    function getUserInfo() {
-        $.ajax({
-            method: 'GET',
-            url: '/my/userinfo',
-            // headers 就是请求头配置对象
-            // headers: {
-            //     Authorization: localStorage.getItem('token') || '',
-            // },
-            success: function (res) {
-                if (res.status !== 0) {
-                    return layui.layer.msg('获取用户信息失败！')
-                }
-                // 调用 renderAvatar 渲染用户的头像
-                renderAvatar(res.data)
-            }
-
-        })
-    }
-
 })
+
+// 获取用户的信息
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        // headers 就是请求头配置对象
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || '',
+        // },
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败！')
+            }
+            // 调用 renderAvatar 渲染用户的头像
+            renderAvatar(res.data)
+        }
+
+    })
+}
