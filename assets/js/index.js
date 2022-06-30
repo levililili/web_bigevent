31697e0aea63050e83037d6b215ecefffcd7ca68
levililili <593494294@qq.com>
2022-06-30 18:20:51 +0800

    修改了index.js，封装了渲染函数

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,6 @@
 $(function () {
-    // 获取用户的信息
-    $.ajax({
-        method: 'GET',
-        url: '/my/userinfo',
-        // headers 就是请求头配置对象
-        // headers: {
-        //     Authorization: localStorage.getItem('token') || '',
-        // },
-        success: function (res) {
-            if (res.status !== 0) {
-                return layui.layer.msg('获取用户信息失败！')
-            }
-            // 调用 renderAvatar 渲染用户的头像
-            renderAvatar(res.data)
-        }
 
-    })
+    getUserInfo()
 
     // 实现退出提示
     let layer = layui.layer
@@ -51,4 +36,25 @@ $(function () {
 
         }
     }
+
+    // 获取用户的信息
+    function getUserInfo() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/userinfo',
+            // headers 就是请求头配置对象
+            // headers: {
+            //     Authorization: localStorage.getItem('token') || '',
+            // },
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layui.layer.msg('获取用户信息失败！')
+                }
+                // 调用 renderAvatar 渲染用户的头像
+                renderAvatar(res.data)
+            }
+
+        })
+    }
+
 })
